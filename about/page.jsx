5f986389cb00ac49ed1df9e57da07862e704c810
Harsha <harshaa.bsc23@rvu.edu.in>
2025-10-06 12:49:38 +0530

    update

---
+++ about/page.jsx
@@ -0,0 +1,112 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <meta name="generator" content="Framer 629c622" />
+        <title>Crevo – Creative Agency Framer Template</title>
+        <meta 
+          name="description" 
+          content="Showcase your agency with Crevo, a sleek and modern Framer template built for creative studios and bold brands. Fully responsive, fast, and easy to customize." 
+        />
+        <meta 
+          name="framer-search-index" 
+          content="https://framerusercontent.com/sites/7FfyVCqnHDYnfvoJCFCIGd/searchIndex-VGRpVQI7vCJ9.json" 
+        />
+        <link 
+          href="https://framerusercontent.com/images/2JyWcK6dgEUZ7xG33HN3C7c2xk.png" 
+          rel="icon" 
+          media="(prefers-color-scheme: light)" 
+        />
+        <link 
+          href="https://framerusercontent.com/images/2JyWcK6dgEUZ7xG33HN3C7c2xk.png" 
+          rel="icon" 
+          media="(prefers-color-scheme: dark)" 
+        />
+        <link 
+          rel="apple-touch-icon" 
+          href="https://framerusercontent.com/images/L5hlbtDoaKOFdtpcREAavBSJ6c.png" 
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Crevo – Creative Agency Framer Template" />
+        <meta 
+          property="og:description" 
+          content="Showcase your agency with Crevo, a sleek and modern Framer template built for creative studios and bold brands. Fully responsive, fast, and easy to customize." 
+        />
+        <meta 
+          property="og:image" 
+          content="https://framerusercontent.com/assets/wQq1OZd05DSSaTdM6FpQDLAhH6g.jpg" 
+        />
+      </Head>
+
+      <div 
+        id="main" 
+        data-framer-hydrate-v2='{"routeId":"gDigcvG1Z","localeId":"default","breakpoints":[{"hash":"19lb7he","mediaQuery":"(min-width: 1200px)"},{"hash":"c0x7nd","mediaQuery":"(min-width: 768px) and (max-width: 1199px)"},{"hash":"1a2ryly","mediaQuery":"(max-width: 767px)"},{"hash":"1uey4f7","mediaQuery":"(min-width: 1200px)"},{"hash":"lskuhs","mediaQuery":"(min-width: 768px) and (max-width: 1199px)"},{"hash":"1u78x6j","mediaQuery":"(max-width: 767px)"}]}'
+        data-framer-ssr-released-at="2025-06-12T12:32:04.789Z"
+        data-framer-page-optimized-at="2025-06-23T09:00:13.094Z"
+        data-framer-generated-page
+      >
+        <style jsx global>{`
+          :root body { 
+            background: var(--token-0d0010c4-42d2-46de-a79f-824af5dfd1e1, rgb(10, 10, 10)); 
+          }
+        `}</style>
+        
+        <div className="framer-kGIHF framer-1uey4f7" data-layout-template="true" style={{minHeight: '100vh', width: 'auto'}}>
+          <div className="framer-6pijwb-container" data-framer-layout-hint-center-x="true" data-framer-name="Navigation" name="Navigation">
+            <div className="ssr-variant hidden-lskuhs hidden-1u78x6j">
+              <header name="Navigation" className="framer-32MvD framer-DdEtp framer-dWm0a framer-p3ct21 framer-v-p3ct21" data-framer-name="Navigation" style={{width: '100%'}}>
+                <div className="framer-1584cud" data-framer-name="Nav">
+                  <div className="framer-1xtrer" data-framer-name="Brand Wrap">
+                    <Link href="./" className="framer-1nlza5t framer-i8n6d7" data-framer-name="Brand">
+                      <div className="framer-10ac813" data-framer-name="Logo">
+                        <div style={{position: 'absolute', borderRadius: 'inherit', top: 0, right: 0, bottom: 0, left: 0}} data-framer-background-image-wrapper="true">
+                          <img 
+                            decoding="async" 
+                            width="39" 
+                            height="50" 
+                            src="https://framerusercontent.com/images/FLaVAZYOy4ttSxd8hV95j8v0E.svg" 
+                            alt="Brand logo" 
+                            style={{display: 'block', width: '100%', height: '100%', borderRadius: 'inherit', objectPosition: 'center', objectFit: 'cover'}} 
+                          />
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                  <div className="framer-pesfnn" data-framer-name="Nav Location">
+                    <div className="framer-177okzh" style={{outline: 'none', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', flexShrink: 0, '--framer-link-text-color': 'rgb(0, 153, 255)', '--framer-link-text-decoration': 'underline', transform: 'none'}} data-framer-component-type="RichTextContainer">
+                      <p className="framer-text framer-styles-preset-dg3sqm" data-styles-preset="OTgx0uDmk">12.9629° N, 77.5775° E</p>
+                    </div>
+                    <div className="framer-an0f0x" style={{outline: 'none', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', flexShrink: 0, '--extracted-r6o4lv': 'var(--token-da7df007-dfe0-4abe-a074-251d360ec7e7, rgb(255, 255, 255))', '--framer-link-text-color': 'rgb(0, 153, 255)', '--framer-link-text-decoration': 'underline', transform: 'none'}} data-framer-component-type="RichTextContainer">
+                      <p className="framer-text framer-styles-preset-wk993t" data-styles-preset="iyqlgulWa" style={{'--framer-text-color': 'var(--extracted-r6o4lv, var(--token-da7df007-dfe0-4abe-a074-251d360ec7e7, rgb(255, 255, 255)))'}}>Studio Based on India</p>
+                    </div>
+                  </div>
+                  <div className="framer-w0qn82" data-framer-name="Nav Social">
+                    <div className="framer-17qdbqo" style={{outline: 'none', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', flexShrink: 0, '--framer-link-text-color': 'rgb(0, 153, 255)', '--framer-link-text-decoration': 'underline', transform: 'none'}} data-framer-component-type="RichTextContainer">
+                      <p className="framer-text framer-styles-preset-dg3sqm" data-styles-preset="OTgx0uDmk">Social media</p>
+                    </div>
+                    <div className="framer-15yu9ra" data-framer-name="Nav Social List">
+                      <div className="framer-18jvqj5-container">
+                        <a className="framer-fwR5R framer-1qsij6v framer-v-1qsij6v framer-14zm6vy" data-framer-name="Social Link" style={{opacity: 1}} href="https://www.linkedin.com/in/zephyro-web-81153b362/" target="_blank" rel="noopener">
+                          <div className="framer-14r3zg" style={{outline: 'none', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', flexShrink: 0, '--extracted-r6o4lv': 'var(--token-da7df007-dfe0-4abe-a074-251d360ec7e7, rgb(255, 255, 255))', '--framer-link-text-color': 'rgb(0, 153, 255)', '--framer-link-text-decoration': 'underline', transform: 'none'}} data-framer-component-type="RichTextContainer">
+                            <p style={{'--font-selector': 'R0Y7R2Vpc3QtNjAw', '--framer-font-family': '"Geist", "Geist Placeholder", sans-serif', '--framer-font-weight': '600', '--framer-line-height': '0.9em', '--framer-text-color': 'var(--extracted-r6o4lv, var(--token-da7df007-dfe0-4abe-a074-251d360ec7e7, rgb(255, 255, 255)))'}} className="framer-text">LN</p>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </header>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <script async src="https://events.framer.com/script?v=2" data-fid="a64dec623177f8d1b46ec91a93cbb3428ff8c6f0f6a73706cb4611c99d5459a2" data-no-nt></script>
+    </>
+  );
+}
